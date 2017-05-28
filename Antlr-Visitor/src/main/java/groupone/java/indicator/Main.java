30eda4ff2b6f41d3147e3c698876a3cd1a747a18
Matias Timossi <mftimossi@gmail.com>
2017-05-28 18:32:29 -0300

    add ut to antlr visitor

---
+++ Antlr-Visitor/src/main/java/groupone/java/indicator/Main.java
@@ -0,0 +1,29 @@
+package groupone.java.indicator;
+//package dds;
+
+
+import grammar.*;
+import java.lang.Exception;
+import org.antlr.v4.runtime.ANTLRInputStream;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.tree.ParseTree;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+
+//        if (args.length == 0) {
+//            args = new String[]{"src/main/java/test."};
+//        }
+    	String nuevoIndicador = "OtroIndicador=50\r\n(2*2)+OtroIndicador\r\n";
+    	
+        System.out.println("parsing: " + nuevoIndicador);
+
+        IndicatorGrammarLexer lexer = new IndicatorGrammarLexer(new ANTLRInputStream(nuevoIndicador));
+        IndicatorGrammarParser parser = new IndicatorGrammarParser(new CommonTokenStream(lexer));
+        ParseTree tree = parser.prog();
+        EvalVisitor visitor = new EvalVisitor();
+        visitor.visit(tree);
+
+    }
+}

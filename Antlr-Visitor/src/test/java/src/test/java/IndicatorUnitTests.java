f8e302e6fa4d56b9520e7de8d9712e8477c74f6a
Matias Timossi <mftimossi@gmail.com>
2017-05-28 21:44:59 -0300

    Add IndicatorErrorListener

--- Antlr-Visitor/src/test/java/src/test/java/IndicatorUnitTests.java
+++ Antlr-Visitor/src/test/java/src/test/java/IndicatorUnitTests.java
@@ -3,25 +3,34 @@ package src.test.java;
 import static org.junit.Assert.*;
 import org.antlr.v4.runtime.ANTLRInputStream;
 import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.misc.ParseCancellationException;
 import org.antlr.v4.runtime.tree.ParseTree;
 import org.junit.Assert;
 import org.junit.Test;
 import grammar.IndicatorGrammarLexer;
 import grammar.IndicatorGrammarParser;
 import groupone.java.indicator.EvalVisitor;
+import groupone.java.indicator.IndicatorErrorListener;
 
 public class IndicatorUnitTests {
 	@Test
-	public void testGramatica() {
-		String nuevoIndicador = "OtroIndicador=50\r\n(2*2)+OtroIndicador\r\n";
+	public void testGramatica() throws Exception {
+		String nuevoIndicador = "OtroIndicador=50\r\n(2dasljadsjkas*2)+OtroIndicador\r\n";
 
-		System.out.println("parsing: " + nuevoIndicador);
-
+		System.out.println("parsing: " + nuevoIndicador);		
+		IndicatorErrorListener indicatorErrorListener = new IndicatorErrorListener(); 
 		IndicatorGrammarLexer lexer = new IndicatorGrammarLexer(new ANTLRInputStream(nuevoIndicador));
+		lexer.addErrorListener(indicatorErrorListener);
 		IndicatorGrammarParser parser = new IndicatorGrammarParser(new CommonTokenStream(lexer));
+		parser.addErrorListener(indicatorErrorListener);
 		ParseTree tree = parser.prog();
 		EvalVisitor visitor = new EvalVisitor();
-		visitor.visit(tree);
+		
+		try{
+			visitor.visit(tree);
+		}catch(final ParseCancellationException e){
+			throw new Exception(e.getMessage());
+		}
 
 		Assert.assertTrue(true);
 	}

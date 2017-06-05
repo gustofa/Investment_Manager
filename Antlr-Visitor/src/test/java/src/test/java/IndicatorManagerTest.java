45c0d01b076e0e4e0e96448ea735a2fd6bf52bac
matiasmmerlo <matimerlo@est.frba.utn.edu.ar>
2017-06-04 21:12:17 -0300

    Messages errors on IndicatorManagerTest

--- Antlr-Visitor/src/test/java/src/test/java/IndicatorManagerTest.java
+++ Antlr-Visitor/src/test/java/src/test/java/IndicatorManagerTest.java
@@ -8,8 +8,6 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 import org.junit.rules.TemporaryFolder;
-
-import groupone.java.investment.AccountManager;
 import groupone.java.investment.Indicator;
 import groupone.java.investment.IndicatorManager;
 import groupone.java.investment.IndicatorSyntaxException;

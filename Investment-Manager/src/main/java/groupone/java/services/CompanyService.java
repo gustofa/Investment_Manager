8b9b77c125bc1c930180ca24ce132af632addeec
ingridGrc <ivonne.grc20@gmail.com>
2017-09-03 21:45:58 -0300

    Refactor CompanyService

--- Investment-Manager/src/main/java/groupone/java/services/CompanyService.java
+++ Investment-Manager/src/main/java/groupone/java/services/CompanyService.java
@@ -1,14 +1,32 @@
 package groupone.java.services;
+import java.util.Collection;
+import java.util.HashMap;
+
 import groupone.java.bean.Account;
 import groupone.java.bean.Company;
 
 public class CompanyService {
+	
+	private static CompanyService instance;
+	private HashMap<String, Company> companies = new HashMap<String, Company>();
+	
+	public static CompanyService getInstance() {
+		if (instance == null) {
+			instance = new CompanyService();
+		}
+
+		return instance;
+	}
 	
 	public Account getAccount(String name, String year, Company company){	
 		return company.getAccounts().stream()
 				.filter(a ->  a.getName().equals(name) && a.getYear().equals(year))
 				.findFirst()
 				.orElse(null);	
+	}
+	
+	public Collection<Company> getCompanies(){
+		return this.companies.values();
 	}
 
 }

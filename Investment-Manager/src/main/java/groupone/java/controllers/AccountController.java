7b2d55b9c334c46f607437035df57d84a61e7e6f
gustofa <gustofa@gmail.com>
2017-09-29 19:57:14 -0300

    Agrego capa controlador a Account

---
+++ Investment-Manager/src/main/java/groupone/java/controllers/AccountController.java
@@ -0,0 +1,55 @@
+package groupone.java.controllers;
+
+import static spark.Spark.get;
+import static spark.Spark.post;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import groupone.java.bean.Account;
+import groupone.java.bean.Company;
+import groupone.java.services.AccountService;
+import groupone.java.services.CompanyService;
+import spark.ModelAndView;
+import spark.TemplateViewRoute;
+
+public class AccountController {
+	private static String layout = "index.vtl";
+	
+	   //Vista: Listado de Cuentas
+	public static TemplateViewRoute serveAccountsPage = (request, response) -> {
+		Boolean addConfirmed = Boolean.parseBoolean(request.queryParams("confirmed"));
+    	Map<String, Object> model = new HashMap<String, Object>();
+    	AccountService serviceAccount = AccountService.getInstance();
+    	model.put("accounts", serviceAccount.getAccounts());
+    	model.put("addConfirmed", addConfirmed);
+    	model.put("template", "Views/Account/accounts.vtl" );
+        return new ModelAndView(model, layout);
+    };     
+    
+    //Vista: Crear Cuenta
+    public static TemplateViewRoute serveCreateAccountPage = (request, response) -> {
+    	Map<String, Object> model = new HashMap<String, Object>();
+    	CompanyService serviceCompany = CompanyService.getInstance();
+    	model.put("companies", serviceCompany.getCompanies());
+    	model.put("template", "Views/Account/account.vtl" );
+        return new ModelAndView(model, layout);
+    };     
+    
+    public static TemplateViewRoute handleCreateAccountPost = (request, response) -> {
+    	AccountService serviceAccount = AccountService.getInstance();
+    	String name = request.queryParams("name");
+    	String year = request.queryParams("year");
+    	Double value = Double.parseDouble(request.queryParams("value"));
+    	Long id = Long.parseLong(request.queryParams("company"));
+       	Company company = CompanyService.getInstance().getCompanies().stream()
+				.filter(a -> a.getId().equals(id) )
+				.findFirst()
+				.orElse(null); 
+    	serviceAccount.createAccount(name, year, value, company);
+    	response.redirect("/accounts?confirmed=true");
+    	return null;
+    };   
+	
+	
+}
--- Investment-Manager/src/main/java/groupone/java/views/App.java
+++ Investment-Manager/src/main/java/groupone/java/views/App.java
@@ -8,6 +8,7 @@ import javax.persistence.Persistence;
 
 import groupone.java.bean.Account;
 import groupone.java.bean.Company;
+import groupone.java.controllers.AccountController;
 import groupone.java.controllers.IndicatorController;
 import groupone.java.repositories.Repository;
 import groupone.java.services.AccountService;
@@ -21,23 +22,12 @@ import static spark.Spark.*;
 
 public class App {
 	private static final String PERSISTENCE_UNIT_NAME = "DDS";
-	private static EntityManagerFactory emFactory;
-	
-	 public static void main( String[] args ) {
 
-			emFactory = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
-			Repository repository = new Repository(emFactory.createEntityManager());		 
-		 
-	        //Instanciamos Servicio para Compañias
-	        CompanyService serviceCompany = CompanyService.getInstance();
-			//repository.companies().getCompanies();
+	public static void main( String[] args ) {
 
-			//Instanciamos Servicio para Cuentas
-			AccountService serviceAccount = AccountService.getInstance();
-			//repository.accounts().getAccounts();
-			
 		 	Spark.staticFileLocation("public");	
 		 	Spark.port(9002);
+		 	
 		 	//página base que contendrá a todas las demás
 		    String layout = "index.vtl";
 
@@ -46,44 +36,10 @@ public class App {
 		      model.put("template", "home.vtl" );
 		      return new ModelAndView(model, layout);
 		    }, new VelocityTemplateEngine());	          
-	    	        
-		    
-		   //Vista: Listado de Cuentas
-		    get("/accounts", (request, response) -> {
-	        	Map<String, Object> model = new HashMap<String, Object>();
-	        	model.put("accounts", serviceAccount.getAccounts());
-	        	model.put("template", "Views/Account/accounts.vtl" );
-	            return new ModelAndView(model, layout);
-	        }, new VelocityTemplateEngine());     
-		    
-		    //Vista: Crear Cuenta
-	        get("/account", (request, response) -> {
-	        	Map<String, Object> model = new HashMap<String, Object>();
-	        	model.put("companies", serviceCompany.getCompanies());
-	        	model.put("template", "Views/Account/account.vtl" );
-	            return new ModelAndView(model, layout);
-	        }, new VelocityTemplateEngine());     
-	        
-	        post("/create_account", (request, response) -> {
-	        	Map<String, Object> model = new HashMap<String, Object>();
-
-	        	String name = request.queryParams("name");
-	        	String year = request.queryParams("year");
-	        	Double value = Double.parseDouble(request.queryParams("value"));
-	        	Long id = Long.parseLong(request.queryParams("company"));
-	        	Company company = serviceCompany.getCompanies().stream()
-	    				.filter(a -> a.getId().equals(id) )
-	    				.findFirst()
-	    				.orElse(null); 
-	        	
-	        	Account account = serviceAccount.createAccount(name, year, value, company);
-
-	        	model.put("template", "Views/Account/create_ok.vtl" );
-	        	model.put("title", "Cuentas - Administraci&oacute;n");
-	        	model.put("subtitle", "Creaci&oacute;n de Cuentas");
-	        	model.put("account", account);
-	            return new ModelAndView(model, layout);
-	        }, new VelocityTemplateEngine());   
+	    	            
+		    get("/accounts",AccountController.serveAccountsPage, new VelocityTemplateEngine());     
+	        get("/account",AccountController.serveCreateAccountPage, new VelocityTemplateEngine());     
+	        post("/account", AccountController.handleCreateAccountPost , new VelocityTemplateEngine());   
 	        
 	        get("/indicators", IndicatorController.serveIndicatorsPage, new VelocityTemplateEngine());
 	        get("/indicator", IndicatorController.serveCreateIndicatorPage, new VelocityTemplateEngine());

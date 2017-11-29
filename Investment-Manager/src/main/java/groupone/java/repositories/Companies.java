c47f90a6edcc1ecdd77ec8ef9d06ba67d9e61da1
ingridGrc <ivonne.grc20@gmail.com>
2017-11-29 00:20:26 -0300

    Actualizar cuentas bach

--- Investment-Manager/src/main/java/groupone/java/repositories/Companies.java
+++ Investment-Manager/src/main/java/groupone/java/repositories/Companies.java
@@ -1,10 +1,9 @@
 package groupone.java.repositories;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.EntityManager;
-
-import groupone.java.bean.Account;
 import groupone.java.bean.Company;
 
 public class Companies extends Repository {
@@ -22,6 +21,12 @@ public class Companies extends Repository {
 		em.getTransaction().commit();
 	}
 	
+	public void update(Company company) {
+		em.getTransaction().begin();
+		em.persist(company);
+		em.getTransaction().commit();
+	}
+	
 	@SuppressWarnings("unchecked")
 	public List<Company> getCompanies() {
 		List<Company> companies = null;
@@ -29,5 +34,18 @@ public class Companies extends Repository {
 				.getResultList();
 		return companies;
 		}	
+	
+	public Company getCompanyByName(String name) {
+		Company company = null;
+		List<Company> companies = new ArrayList<Company>();
+		companies =  em.createQuery("SELECT c FROM Company c WHERE name=:name")
+				.setParameter("name", name)
+				.getResultList();
+			if (companies.size()>0) {
+				return companies.get(0);
+			} else {
+				return company;
+			}
+		}
 	
 }
